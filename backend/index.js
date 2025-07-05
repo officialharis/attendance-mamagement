91a3d2f3bec0cf59e00a505eed71e2beea982e1c
BahauddinRafiuddin <bahauddinrafiuddin@gmail.com>
2025-07-05 18:24:46 +0530

    Making Project To Run Locally

--- backend/index.js
+++ backend/index.js
@@ -13,13 +13,27 @@ dotenv.config()
 const app = express()
 const port = process.env.PORT || 4000
 
-// Allow only your frontend domain
-const corsOptions = {
-  origin: 'https://attendance-management-frontend-vsnq.onrender.com',
-  credentials: true, // if you use cookies or auth headers
-};
+// Alllowed origin
+const allowedOrigins = [
+  "http://localhost:5173", // development
+  "https://attendance-management-frontend-vsnq.onrender.com"  //Production
+]
 
-app.use(cors(corsOptions));
+// Allow only your frontend domain
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // allow requests with no origin (like mobile apps, curl, Postman)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        return callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 
 
 // const allowedOrigin = [
@@ -33,17 +47,18 @@ await connectDatabase();
 await cloudinaryConnect();
 
 app.get('/', (req, res) => {
-    res.send("Api Is Working..")
+  res.send("Api Is Working..")
 })
 
 // Diffrent Routes
-app.use('/api/admin',adminRouter)
-app.use('/api/user',userRouter)
-app.use('/api/classroom',classroomRouter)
-app.use('/api/subject',subjectRouter)
-app.use('/api/attendance',attendanceRouter)
+app.use('/api/admin', adminRouter)
+app.use('/api/user', userRouter)
+app.use('/api/classroom', classroomRouter)
+app.use('/api/subject', subjectRouter)
+app.use('/api/attendance', attendanceRouter)
 
 // Listining Server
-app.listen(port, (req, res) => {
-   console.log(`Server Is Running On http://localhost:${port}`);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, (req, res) => {
+  console.log(`Server Is Running On http://localhost:${port}`);
 })
